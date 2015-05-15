f2573f1ac4f2db78eb98ad60c2fc338c4aaf83ed
rafael <rafarocar_@hotmail.com>
2015-05-15 11:07:38 -0300

    INT: Parece que ta funfando

--- rubyClient.rb
+++ rubyClient.rb
@@ -1,43 +1,124 @@
-require "socket"
+require "net/telnet"
 
-class Client
-  def initialize( server )
-    @server = server
-    @request = nil
-    @response = nil
-    listen
-    send
-    @request.join
-    @response.join
-  end
- 
-  def listen
-    @response = Thread.new do
-      loop {
-        msg = @server.gets.chomp
-        puts "#{msg}"
-      }
-    end
-  end
- 
-  def send(msg="Default Message To Socket")
-    @request = Thread.new do
-      loop {
-        @server.puts( msg )
-      }
-    end
-  end
-end
- 
-server = TCPSocket.open( "localhost", 3000 )
-
-c=Client.new( server )
-c.send
-
-while true
-	puts "Send a Message to server:\n"
-	msg = $stdin.read
-	c.send(msg)	
+def sendMsg(servers,msg)
+	servers.each do |server|
+		server.puts(msg)
+	end
 end
 
+def verifyFiles()
+	finding=true
+	i=0;
+	while finding
+		if(File.exist?('logClient'+i+'.log')		
+			i++
+		else
+			return i;
+		end
+	end
+end
 
+def verifyDatas(datas,msg)
+	datas.each do |data|
+		if(data!=msg && data.chomp!=msg)
+			return false
+		end
+		
+	end
+	return true
+end
+
+def waitFor(server)
+	server.waitfor(/./) do |data|
+		if(data=="ACK" || data=="OK" || data=="NOK")
+			return data
+		end
+	end
+end
+
+fileNumber=verifyFiles()
+
+file= File.open('logClient'+fileNumber+'.log', 
+				File::WRONLY | File::APPEND)
+logger= Logger.new(file)
+logger.datetime_format="%d-%m-%Y %H:%M:%S"
+logger.formatter = proc do |severity,datetime,progname,msg|
+	"[#{datetime}]#{progname}:#{severity}=>#{msg}\n"
+end
+
+logger.info "Getting Port for servers"
+puts "Digite a porta do servidor 1"
+port1= gets
+logger.info "Server1 has port:"+port1
+puts "Digite a porta do servidor 2"
+port2= gets
+logger.info "Server1 has port:"+port2
+puts "Digite a porta do servidor 3"
+port3= gets
+logger.info "Server1 has port:"+port3
+
+logger.info "Connecting to servers..."
+
+servers=[Net::Telnet::new("Host"=>"localhost","Port" =>Integer(port1),"Telnetmode"=>false),
+		Net::Telnet::new("Host"=>"localhost","Port" =>Integer(port2),"Telnetmode"=>false),
+		Net::Telnet::new("Host"=>"localhost","Port" =>Integer(port3),"Telnetmode"=>false)]
+
+logger.info "Connection to servers sucessfull"
+
+
+lines_to_send=['Hello!','Send a message','Bye']
+
+lines_to_send.each do |line|
+
+
+	sendMsg(servers,"change")
+
+	data1=waitFor(servers[0])
+	data2=waitFor(servers[1])
+	data3=waitFor(servers[2])
+
+	datas=[data1,data2,data3]
+
+	puts data1
+	puts data2
+	puts data3
+
+	if(verifyDatas(datas,"OK"))
+
+		sendMsg(servers,"commit")
+
+		data1=waitFor(servers[0])
+		data2=waitFor(servers[1])
+		data3=waitFor(servers[2])
+
+		datas=[data1,data2,data3]
+
+		puts data1
+		puts data2
+		puts data3
+
+		if(verifyDatas(datas,"OK"))
+			puts("Sending Data")
+			sendMsg(servers,"data:"+line)
+
+			data1=waitFor(servers[0])
+			data2=waitFor(servers[1])
+			data3=waitFor(servers[2])
+
+			datas=[data1,data2,data3]
+
+			puts data1
+			puts data2
+			puts data3
+			sleep(1.0/3.0)
+			if(verifyDatas(datas,"NACK"))
+				sendMsg(servers,"abort")
+			end
+		else
+			puts("Sending Abort")
+			sendMsg(servers,"abort")
+		end
+	else
+		sendMsg(servers,"abort")
+	end
+end
--- rubyServer.rb
+++ rubyServer.rb
@@ -1,14 +1,39 @@
-require 'socket'      # Sockets are in standard library
+require 'socket'
 
-hostname = '127.0.0.1'
-port = 8500
+puts "Digite a porta do servidor"
+port= gets
 
-s = TCPSocket.open(hostname, port)
+server = TCPServer.new("127.0.0.1",Integer(port))
 
-while true
-	while line = s.gets   # Read lines from the socket
-	  puts line.chop      # And print with platform line terminator
+data="Dado1"
+ok=true;
+
+loop do
+	Thread.start(server.accept) do |client|
+		while line = client.gets   # Read lines from the socket
+			puts line.chomp      # And print with platform line terminator
+			if(((line.chomp == "change") || (line == "change")) && ok)
+				puts("Receive Change")
+				client.puts "OK"
+				ok=false
+			elsif(((line.chomp == "change") || (line == "change")) && !ok)
+				puts("Receive Change but i can't change")
+				client.puts "NOK"
+			elsif((line.chomp == "abort") || (line == "abort"))
+				puts("Receive Abort")
+				client.puts "ACK"
+				ok=true
+			elsif((line.chomp == "commit") || (line == "commit"))
+				puts("Receive commit")
+				client.puts "OK"
+			elsif((line.include? "data:") || (line.chomp.include? "data:"))
+				puts("Receive data, change data from "+data)
+				dados=line.split("data:")
+				data=dados[1]
+				ok=true
+				client.puts "ACK"
+				puts("To "+data)
+			end
+		end
 	end
 end
-
-s.close               # Close the socket when done
